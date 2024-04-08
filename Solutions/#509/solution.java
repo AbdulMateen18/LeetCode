af32942a42d3faab0c1d41cf23cff2a3e66c01a6
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2024-04-08 18:02:59 +0500

    Update solution.java

--- Solutions/#509/solution.java
+++ Solutions/#509/solution.java
@@ -1,13 +1,12 @@
 class Solution {
-    public int fib(int n) {
+    public int fibonacci(int n) {
         if(n==0){
             return 0;
         }
-
         if(n==1){
             return 1;
         }else{
-            return fib(n-1)+fib(n-2);
+            return fibonacci(n-1)+fibonacci(n-2);
         }
     }
 }
