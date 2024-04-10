656ccc32d72c06519a45127a403685a429e6f4ce
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2024-04-10 19:07:07 +0500

    Create solution.java

---
+++ Solutions/#235/solution.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+class Solution {
+   
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> l = new LinkedList<Integer>();
+        helper(root,l);
+        return l;
+    }
+    public void helper(TreeNode root,List l){
+        if(root!=null){
+            helper(root.left,l);
+            l.add(root.val);
+            helper(root.right,l);
+        }
+        
+    }
+}
