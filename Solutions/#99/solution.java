1c0e4122eecb183a6306881543076e54a8b9edf8
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2024-04-11 12:26:33 +0500

    99
    Recover BST

---
+++ Solutions/#99/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+        TreeNode first = null,second=null,prev=null;
+    public void recoverTree(TreeNode root) {
+        
+        inOrder(root);
+        int temp = first.val;
+        first.val = second.val;;
+        second.val = temp;
+    }
+
+    public void inOrder(TreeNode root){
+        if(root==null){
+            return;
+        }else{
+            inOrder(root.left);
+            if(prev!=null && root.val<prev.val){
+                if(first==null){
+                    first=prev;
+                }
+                second=root;
+            }
+            prev = root;
+            inOrder(root.right);
+        }
+    }
+}
