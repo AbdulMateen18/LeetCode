9549702276579ea9afe77aeca083ed1bf8a734d6
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2024-04-19 11:32:06 +0500

    Update solution.java

--- Solutions/#1637/solution.java
+++ Solutions/#1637/solution.java
@@ -3,8 +3,8 @@ class Solution {
         //Sort 2D array on the basis of first element/x-coordinate
         Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
         int max_width = 0;
-
-        for (int j = 1; j < points.length; j++) {
+        int n = points.length;
+        for (int j = 1; j < n; j++) {
             int width = points[j][0] - points[j - 1][0];
             max_width = Math.max(max_width, width);
         }
