be5700114593e533b2c538a5dd34a5bb030080af
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2024-04-19 11:33:05 +0500

    Update solution.java

--- Solutions/#2706/solution.java
+++ Solutions/#2706/solution.java
@@ -2,7 +2,8 @@ import java.util.Arrays;
 class Solution {
     public int buyChoco(int[] prices, int money) {
         Arrays.sort(prices);
-        for(int i=0;i<prices.length-1;i++){
+        int n = prices.length;
+        for(int i=0;i<n-1;i++){
             if(prices[i]+prices[i+1]<=money){
                 money = money-(prices[i]+prices[i+1]);
                 break;
