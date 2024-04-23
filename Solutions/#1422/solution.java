4aba207cf5360f3fe31fc23212db1ea77a0330c3
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2024-04-23 17:08:13 +0500

    added 1422

--- Solutions/#1422/solution.java
+++ Solutions/#1422/solution.java
@@ -1,1 +1,11 @@
-f
+class Solution {
+    public int maxScore(String s) {
+        int ones=0,zeros=0,max=Integer.MIN_VALUE;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='1') ones++;
+            else zeros++;
+            if(i!=s.length()-1) max = Math.max(zeros-ones,max);
+        }
+        return max+ones;
+    }
+}
