a6d84ff4e61ac6d247da90674e45f111a561663b
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2024-04-24 15:43:36 +0500

    Update solution.java

--- Solutions/#342/solution.java
+++ Solutions/#342/solution.java
@@ -1,9 +1,8 @@
 class solution {
     public boolean isPowerOfFour(int n) {
         if(n==0) return false;
-        else if(n==1){
-            return true;
-        }else{
+        else if(n==1) return true;
+        else{
             return n%4==0 && isPowerOfFour(n/4);
         }
     }
