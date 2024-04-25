be1b3ffccfad63c6b538b471d0148d5334912a9f
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2024-04-25 19:02:36 +0500

    Update solution.java

--- Solutions/#231/solution.java
+++ Solutions/#231/solution.java
@@ -1,9 +1,8 @@
 class solution {
     public boolean isPowerOfTwo(int n) {
         if(n==0) return false;
-        else if(n==1){
-            return true;
-        }else{
+        else if(n==1) return true;
+        else{
             return n%2==0 && isPowerOfTwo(n/2);
         }   
     }
