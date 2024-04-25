bb43ee7a286ea0dde2ac69955842a8457685606f
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2024-04-25 19:00:12 +0500

    Update solution.java

--- Solutions/#235/solution.java
+++ Solutions/#235/solution.java
@@ -3,11 +3,11 @@ class solution {
         int small = Math.min(p.val, q.val);
         int large = Math.max(p.val, q.val);
         while (root != null) {
-            if (root.val > large) // p, q belong to the left subtree
+            if (root.val > large)
                 root = root.left;
-            else if (root.val < small) // p, q belong to the right subtree
+            else if (root.val < small)
                 root = root.right;
-            else // Now, small <= root.val <= large -> This root is the LCA between p and q
+            else
                 return root;
         }
         return null;
