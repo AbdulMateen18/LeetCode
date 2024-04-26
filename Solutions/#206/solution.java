66cab930747dc6cabd53b042665269419491bcb8
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2024-04-26 19:35:56 +0500

    .

--- Solutions/#206/solution.java
+++ Solutions/#206/solution.java
@@ -1,15 +1,13 @@
 class solution {
     public ListNode reverseList(ListNode head) {
-         ListNode prev = null;  
-        ListNode current = head;
-    
-        
+        ListNode prev = null;  
+        ListNode current = head; 
         while(current != null) { 
             ListNode next = current.next; 
             current.next = prev;
             prev = current;
             current = next;
         }
-       return prev; 
+        return prev; 
     }
 }
