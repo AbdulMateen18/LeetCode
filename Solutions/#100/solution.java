a7d1f1e831b097bee3d9502741e00693d4549e17
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2024-04-28 17:17:37 +0500

    Update solution.java

--- Solutions/#100/solution.java
+++ Solutions/#100/solution.java
@@ -2,8 +2,7 @@ class solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p == null && q == null) return true;
         if(p == null || q == null) return false;
-        if(p.val == q.val)
-            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        if(p.val == q.val) return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         return false;
     }
 }
