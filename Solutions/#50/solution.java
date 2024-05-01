f50da1d2db06fa7f79ca9f83b601e5c506c2f8a5
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2024-05-02 00:58:48 +0500

    Update solution.java

--- Solutions/#50/solution.java
+++ Solutions/#50/solution.java
@@ -6,9 +6,7 @@ class solution {
             n= Math.abs(n);
             x=1/x;
         }
-        if(n==0){
-            return 1;
-        }
+        if(n==0) return 1;
         if(n%2==0){
             return myPow(x*x,n/2);
         }else{
