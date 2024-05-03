c0ea50b0b7284df008d96809dc99a7c53d3282f9
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2024-05-04 00:14:46 +0500

    Update solution.java

--- Solutions/#9/solution.java
+++ Solutions/#9/solution.java
@@ -6,6 +6,6 @@ class solution {
     	    rev = rev*10 + x%10;
     	    x = x/10;
         }//while
-    return (x==rev || x==rev/10);
-    }
-}
+        return (x==rev || x==rev/10);
+    }//isPalindrome()
+}//class
