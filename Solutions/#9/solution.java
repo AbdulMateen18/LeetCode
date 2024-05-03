b8fa9ac67fd0577b2a9a183db2df9986deb32182
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2024-05-04 00:13:03 +0500

    Update solution.java

--- Solutions/#9/solution.java
+++ Solutions/#9/solution.java
@@ -1,7 +1,7 @@
 class solution {
     public boolean isPalindrome(int x) {
         if (x<0 || (x!=0 && x%10==0)) return false;
-    int rev = 0;
+        int rev = 0;
     while (x>rev){
     	rev = rev*10 + x%10;
     	x = x/10;
